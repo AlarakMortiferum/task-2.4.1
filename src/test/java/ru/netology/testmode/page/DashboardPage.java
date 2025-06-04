d8b570d6be740bc84cb032b55260385ebc06d570
AlarakMortiferum <sadim1403@gmail.com>
2025-06-04 20:22:54 +0300

    Поправка 3

--- src/test/java/ru/netology/testmode/page/DashboardPage.java
+++ src/test/java/ru/netology/testmode/page/DashboardPage.java
@@ -11,15 +11,18 @@ public class DashboardPage {
     private final ElementsCollection cards = $$(".list__item"); // все карточки
 
     public int getCardBalance(String cardNumber) {
-        SelenideElement card = cards.findBy(text(cardNumber));
-        String text = card.$(".list__item__balance").getText();
-        return extractBalance(text);
+        String lastFourDigits = cardNumber.substring(cardNumber.length() - 4);
+        String cardPartialText = "**** **** **** " + lastFourDigits;
+        SelenideElement card = cards.findBy(text(cardPartialText));
+        String cardText = card.getText();
+        return extractBalance(cardText);
     }
 
     private int extractBalance(String text) {
-        String digits = text.replaceAll("[^0-9]", "");
-        return Integer.parseInt(digits);
+        String balancePart = text.substring(text.indexOf("баланс:") + 7, text.indexOf("р."));
+        return Integer.parseInt(balancePart.trim());
     }
+
 
     public TransferPage selectCardToTransfer(String cardNumber) {
         SelenideElement card = cards.findBy(text(cardNumber));
